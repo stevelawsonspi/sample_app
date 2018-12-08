1fc0a1b0365d4ae88d318f9d684466e0212c4fe0
Steve Lawson <stevelawsonspi@gmail.com>
2018-12-08 06:53:06 +0000

    Initialize repository (Rails tutorial app)

---
+++ db/seeds.rb
@@ -0,0 +1,26 @@
+User.create!(name:  "Steve lawson", email: "stevelawsonspi@gmail.com", 
+             password: "password", password_confirmation: "password", 
+             admin: true, activated: true, activated_at: Time.zone.now)
+99.times do |n|
+  name  = Faker::Name.name
+  email = "stevelawsonspi+example#{n+1}@gmail.com"
+  password = "password"
+  User.create!(name: name, email: email, 
+               password: password, password_confirmation: password,
+              activated: true, activated_at: Time.zone.now)
+end
+
+# Microposts
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
